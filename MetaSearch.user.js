2a80d699f0af21d01297f99229385d9aa9eff537
jus7forfun <a54m4m@gmx.com>
2016-12-06 08:11:19 +0330

    added Apollo

--- MetaSearch.user.js
+++ MetaSearch.user.js
@@ -1454,6 +1454,122 @@ bt.addSource("AvaxHome", function () {
     };
 });
 
+bt.addSource("Apollo", function () {
+    var apollo = "https://apollo.rip/ajax.php?action=browse&searchstr={query}";
+
+    return {
+        website: "https://apollo.rip/",
+        url: {
+            all: apollo,
+            music: apollo + "&filter_cat[1]=1",
+            music_flac: apollo + "&filter_cat[1]=1&format=FLAC",
+            //		music_mp3: apollo + "&filter_cat[1]=1&format=AAC|MP3",
+            elearning: apollo + "&filter_cat[3]=1&filter_cat[4]=1&filter_cat[5]=1&filter_cat[7]=1",
+            mags: apollo + "&filter_cat[3]=1",
+            ebooks: apollo + "&filter_cat[3]=1",
+            fiction: apollo + "&filter_cat[3]=1",
+            abooks: apollo + "&filter_cat[4]=1",
+            comics: apollo + "&filter_cat[7]=1",
+            apps_win: apollo + "&filter_cat[2]=1"
+        },
+        onPrepareQuery: bt.extractGazelleYear,
+        onParse: function (response) {
+            try {
+                var data = JSON.parse(response.responseText);
+            } catch (e) {
+                bt.showFailAlert(response);
+                return null;
+            }
+
+            if (!('status' in data) || data.status !== 'success' || !('response' in data) || !('results' in data.response)) {
+                bt.showFailAlert(response, "unexpected data");
+                return null;
+            }
+
+            return data.response.results;
+        },
+        onFilter: function (data, response) {
+            var words = response.context.query.toLowerCase().split(' ');
+
+            response.context.searchUrl = response.finalUrl.replace('ajax.php?action=browse', 'torrents.php?action=basic');
+
+            return data.filter(function (value) {
+                for (var i = 0, l = words.length; i < l; i++) {
+                    if ((('artist' in value && value.artist.toLowerCase().indexOf(words[i]) === -1) || !('artist' in value)) && value.groupName.toLowerCase().indexOf(words[i]) === -1) {
+                        return false;
+                    }
+                }
+
+                return true;
+            });
+        },
+        onRender: function (data, table) {
+            var nl, group, torrent, groupTable, groupTableHTML, torRow, tr, n, score, cue, ed = "", media = "", link, artist;
+
+            var torrentGroupHeader = bt.trtd.clone();
+            $(torrentGroupHeader[0].firstChild).append(
+                bt.h4.clone().addClass("torrent-group")
+            );
+
+            for (var i = 0, l = data.length; i < l; i++) {
+				ed = ""; media = "";
+                group = data[i];
+                link = "https://apollo.rip/torrents.php?id=" + group.groupId;
+
+                if ("torrents" in group) {
+                    artist = (group.artist === 'Various Artists')
+                        ? 'Various Artists'
+                        : bt.ab.clone().attr("href", "https://apollo.rip/artist.php?id=" + group.torrents[0].artists[0].id).html(group.artist);
+
+                    tr = torrentGroupHeader.clone();
+                    tr[0].firstChild.setAttribute("colspan", "3");
+                    $(tr[0].firstChild.firstChild).append(
+                        artist,
+                        " - ",
+                        bt.ab.clone().attr("href", link).html(group.groupName),
+                        ' [' + group.groupYear + '] [' + group.releaseType + ']'
+                    );
+
+                    if (group.cover) {
+                        $(tr[0].firstChild)
+                            .addClass('cover')
+                            .css('backgroundImage', 'url(' + group.cover + ')');
+                    }
+
+                    groupTableHTML = "";
+                    for (n = 0, nl = group.torrents.length; n < nl; n++) {
+                        torrent = group.torrents[n];
+
+                        if (ed !== torrent.remasterTitle || media !== torrent.media) {
+                            ed = torrent.remasterTitle;
+                            media = torrent.media;
+                            groupTableHTML += '<tr><td colspan="3" class="tr-title">' + (ed ? ed : "Original Release") + ' ' + torrent.remasterCatalogueNumber + ' ' + (torrent.remasterYear ? torrent.remasterYear : "") + ' / ' + media + "</td></tr>";
+                        }
+
+                        score = torrent.hasLog ? ' / Log (' + torrent.logScore + '%)' : '';
+                        cue = torrent.hasCue ? ' / Cue' : '';
+
+                        groupTableHTML += '<tr><td><a href="https://apollo.rip/torrents.php?torrentid=' + torrent.torrentId + '" target="_blank">' + torrent.format + ' / ' + torrent.encoding + score + cue + '</a>' + (torrent.isFreeleech ? ' <span class="label label-success">Freeleech</span>' : '') + '</td><td>' + torrent.seeders + '</td><td><a href="https://apollo.rip/torrents.php?action=download&id=' + torrent.torrentId + '">' + bt.humanizeSize(torrent.size) + '</a></td></tr>';
+                    }
+
+                    groupTable = bt.table.clone()
+                        .addClass("torrent-table")
+                        .html(groupTableHTML);
+
+                    torRow = bt.tr.clone().append(bt.td.clone().attr("colspan", "3"));
+                    torRow[0].firstChild.appendChild(groupTable[0]);
+
+                    table.append(tr, torRow);
+                } else {
+                    table.append('<tr><td><a href="' + link + '" target="_blank">' + group.groupName + '</a></td><td>' + group.seeders + '</td><td><a href="https://apollo.rip/torrents.php?action=download&id=' + group.torrentId + '">' + bt.humanizeSize(group.size) + '</a></td></tr>');
+                }
+            }
+        },
+        icon: "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAQABADASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAEG/8QAJRAAAQMDAQkBAAAAAAAAAAAAAwECBgAEBeEREhMhMUJScaGB/8QAFAEBAAAAAAAAAAAAAAAAAAAABf/EABwRAAAGAwAAAAAAAAAAAAAAAAAREkGh0RNDRP/aAAwDAQACEQMRAD8A0MlHfxaQhytsYz7Mr97hueqtRe5v6nTSrGR38okJsrdGMyzE/e4aPVGqva30iddaSJMnK5CHHAtzisRP2IR41Rq+T+fzWkcTJxWQmxx7c5bEr9ikaNVani/l90pnQyyiwF0PjOaH/9k="
+    };
+});
+
+
 bt.addSource("bB", function () {
     var bb = "https://baconbits.org/torrents.php?action=basic&searchstr={query}";
 
